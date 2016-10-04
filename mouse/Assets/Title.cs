3f6293b1e6f16fe20893888b44e277938c2cfd20
Fukuda Naoto <never9612@gmail.com>
2016-10-04 09:58:34 +0900

    シーン推移の変更 「Title.cs」をScriptフォルダに移動 シーン「mouse.unity」の名前を「GameScene.unity」に変更 タイトル画面からゲーム画面に推移するように変更

---
+++ mouse/Assets/Title.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width / 2, Screen.height / 2, 200, 40), "スタートボタン"))
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+}
