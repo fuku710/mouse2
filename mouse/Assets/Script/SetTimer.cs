5fed0103b107b6a1090da0c1fdf58a3886db2a42
Fukuda Naoto <never9612@gmail.com>
2016-10-11 16:56:25 +0900

    SetTimer.csの修正
    タイマーをカウントダウン方式に変更しました

--- mouse/Assets/Script/SetTimer.cs
+++ mouse/Assets/Script/SetTimer.cs
@@ -4,31 +4,45 @@ using UnityEngine.UI;
 
 public class SetTimer : MonoBehaviour {
 
+	private float totalTime;
 	private int minute;
 	private float second;
+	private float timeCounter;
 	private int oldSecond;
 	//private bool timerFlag = true;
 	private Text textField;
 
 	// Use this for initialization
 	void Start () {
-		minute = 0;
-		second = 0;
+		minute = 1;
+		second = 2f;
+		totalTime = minute * 60 + second;
+		timeCounter = 60;
 		oldSecond = 0;
+		textField = this.GetComponent<Text> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Time.timeScale > 0) {
-			second += Time.deltaTime;
-			if (second >= 60.0f) {
-				minute++;
-				second = second - 60;
-			}
+		/*if (Time.timeScale > 0 && totalTime > 0f) {
+			totalTime = minute * 60 + second;
+			totalTime -= Time.deltaTime;
+
+			minute = (int)(totalTime / 60);
+			second = totalTime - minute * 60;
 			if ((int)second != oldSecond) {
-				this.GetComponent<Text>().text = minute.ToString ("00") + ":" + second.ToString ("00");
+				textField.text = minute.ToString ("00") + ":" + second.ToString ("00");
 			}
 			oldSecond = (int)second;
+
+			if (totalTime <= 0f) {
+				Debug.Log ("制限時間終了");
+			}
 		}
+		*/
+		timeCounter -= Time.deltaTime;
+
+		timeCounter = Mathf.Max (timeCounter, 0.0f);
+		textField.text = ((int)timeCounter).ToString ();
 	}
 }
