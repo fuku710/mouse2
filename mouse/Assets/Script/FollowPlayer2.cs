2447a201614dbb44e5d21571bde60c4e74d6b876
takumil-pg <takumiluigi@yahoo.co.jp>
2016-10-29 01:10:42 +0900

    ・「GameScene2」を追加 ・「FollowPlayer2」やAssetなど追加

---
+++ mouse/Assets/Script/FollowPlayer2.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer2 : MonoBehaviour
+{
+    public GameObject player;
+    private float offset = 5f;
+    public float adjust_y;
+    public float adjust_z;
+
+    void Start ()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + offset + adjust_y, player.transform.position.z - offset + adjust_z);
+    }
+
+    void Update ()
+    {
+
+    }
+}
